e783cc9a45d9d157f9f7aadb3f4d49ecc92198f2
Maxim Kapustyansky <max160996@mail.ru>
2024-04-04 15:39:39 +0300

    Create Task_1_2.py

---
+++ venv/Seminar_6/Task_1_2.py
@@ -0,0 +1,28 @@
+# Создайте модуль с функцией внутри.
+# Функция принимает на вход три целых числа: нижнюю и
+# верхнюю границу и количество попыток.
+# Внутри генерируется случайное число в указанных границах
+# и пользователь должен угадать его за заданное число попыток
+# функция выводит подсказки больше и меньше 
+# если число угадано, возвращается истина, а если попытки исчерпаны - ложь
+
+from random import randint
+
+def guess_number(lower_bound, upper_bound, attempts):
+    number = randint(lower_bound, upper_bound)
+    for i in range(attempts):
+        print(f"Try to guess the number from {lower_bound} to {upper_bound}")
+        guess = int(input())
+        if guess == number:
+            print("You guessed it!")
+            return True
+        elif guess < number:
+            print("Too small")
+        else:
+            print("Too big")
+    print("You've run out of attempts")
+    return False
+
+
+if __name__ == "__main__":
+    guess_number(1, 10, 3)
