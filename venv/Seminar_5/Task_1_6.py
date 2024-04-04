723afe00cd80151b9df2c9bc8652c0f5147b642f
Maxim Kapustyansky <max160996@mail.ru>
2024-04-04 07:43:42 +0300

    Create Task_1_6.py

---
+++ venv/Seminar_5/Task_1_6.py
@@ -0,0 +1,18 @@
+# Введите в консоль таблицу умножения от 2х2 до 9х10 как на школьной тетрадке
+# Таблицу создайте в виде однострочного генератора, где каждый элемент генератора - отдельный пример таблицы умножания.
+# Для вывода результата используйте принт без перехода на новую строку.
+
+MIN = 2
+MAX = 10
+COL = 4
+
+# for i in (MIN, MIN + COL):
+#     for second in range(MIN, MAX + 1):
+#         for first in range(i, i + COL):
+#             print(f'{first :> 6} x {second :> 2} = {first * second :> 3}',end='\t')
+#         print()
+#     print()
+
+table_gen = (f'{first :> 6} x {second :> 2} = {first * second :> 3}'
+             for first in range(MIN, MAX + 1) for second in range(MIN, MAX + 1))
+print(*table_gen, sep='\t')
