3935fd568ec0186049c99df379ca60c8e24cafe1
Maxim Kapustyansky <max160996@mail.ru>
2024-04-04 15:39:31 +0300

    Create Task_1_1.py

---
+++ venv/Seminar_6/Task_1_1.py
@@ -0,0 +1,7 @@
+# Создайте файл, в котором вы импортируете встроенные в модуль функции под псевдонимами. (3-7 строк импорта)
+
+from random import randint as rnd
+from decimal import Decimal as D
+from math import pi as pi
+from math import sqrt as sqrt
+from sys import argv as argv
