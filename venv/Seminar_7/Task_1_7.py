ab0815fe938b03ad4225177ff8cc5fc9f0300354
Maxim Kapustyansky <max160996@mail.ru>
2024-04-27 19:29:09 +0300

    Update Task_1_7.py

--- venv/Seminar_7/Task_1_7.py
+++ venv/Seminar_7/Task_1_7.py
@@ -2,20 +2,23 @@
 # Каждая группа включает файлы с несколькими расширениями.
 # В исходной папке должны остаться только те файлы, которые не подошли для сортировки.
 
-import os
+from os import chdir
+from pathlib import Path
 
 
 def sort_files(path):
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if file.endswith('.mp4') or file.endswith('.mkv'):
-                os.rename(os.path.join(root, file), os.path.join(root, 'video', file))
-            elif file.endswith('.jpg') or file.endswith('.png'):
-                os.rename(os.path.join(root, file), os.path.join(root, 'image', file))
-            elif file.endswith('.txt') or file.endswith('.doc') or file.endswith('.docx'):
-                os.rename(os.path.join(root, file), os.path.join(root, 'text', file))
+    chdir(path)
+    for file in Path(path).iterdir():
+        if file.is_file():
+            if file.suffix in ('.mp4', '.avi', '.mkv'):
+                file.rename(f'{file.parent}/video/{file.name}')
+            elif file.suffix in ('.jpg', '.jpeg', '.png', '.gif'):
+                file.rename(f'{file.parent}/image/{file.name}')
+            elif file.suffix in ('.txt', '.doc', '.docx', '.pdf'):
+                file.rename(f'{file.parent}/text/{file.name}')
             else:
-                os.rename(os.path.join(root, file), os.path.join(root, 'other', file))
+                file.rename(f'{file.parent}/other/{file.name}')
 
 
-# sort_files('C:\\Users\\User\\Desktop\\Seminar_7\\Task_1_7')
+if __name__ == '__main__':
+    sort_files('C:\Users\max16\PycharmProjects\seminarGB\venv\python_again\venv\Seminar_7')
