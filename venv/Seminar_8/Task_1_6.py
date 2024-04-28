a20edaf0f1e37c1ea623f458f9b7cececfd69b88
Maxim Kapustyansky <max160996@mail.ru>
2024-04-28 22:19:22 +0300

    Create Task_1_6.py

---
+++ venv/Seminar_8/Task_1_6.py
@@ -0,0 +1,25 @@
+"""
+Напишите функцию, которая преобразует pickle файл хранящий список словарей в табличный csv файл.
+Для тестирования возьмите pickle версию файла из задачи 4 этого семинара.
+Функция должна извлекать ключи словаря для заголовков столбца из переданного файла.
+"""
+import csv
+import json
+import pickle
+from pathlib import Path
+
+
+def pickle_to_csv(file: Path) -> None:
+    with(
+        open(file, 'rb') as f_read,
+        open(f'{file.stem}.csv', 'w', encoding='utf-8') as f_write
+    ):
+        data = pickle.load(f_read)
+        headers = list(data[0].keys())
+        csv_write = csv.DictWriter(f_write, fieldnames=headers, dialect='excel-tab', quoting=csv.QUOTE_NONNUMERIC)
+        csv_write.writeheader()
+        csv_write.writerows(data)
+
+
+if __name__ == '__main__':
+    pickle_to_csv(Path('data.pickle'))
